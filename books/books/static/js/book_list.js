c0176c9ac76c6327f8d1c881b40775ddd4d1a52b
Cătălin Badea <catalin.badea392@gmail.com>
2013-10-19 21:47:19 +0300

    we has books.

--- books/books/static/js/book_list.js
+++ books/books/static/js/book_list.js
@@ -1,13 +1,19 @@
-var handle_bar_template = {}
-
 $(function() {
-  callFact();
-});
-$.get( "static/book_entry_template.html", function( data ) {
-	handle_bar_template = data;
-});
 
-$.get("/api/v1/books/?format=json"), function ( data ) {
-	alert("merge");
-}
+	$.get("api/v1/books/?format=json", function ( data ) {
+		$.get("static/book_entry_template.html", function ( template_info ) {
+			var template = Handlebars.compile(template_info);
+			var books = data.objects;
+			for (var i in books) {
+				console.log(books[i]);
+				$(".book_list").append(template(books[i]));
+			}
+		});
+	});
 
+	$.get( "static/handlebar_test_template.html", function( data ) {
+		var template = Handlebars.compile(data);
+		var context = {title: "My New Post", body: "This is my first post!"}
+		$(".list").html(template(context));
+	});
+});
