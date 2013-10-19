a4079b84e7a2227c96825b748199277de9a3d0af
Sorina Sandu <sandu.sorina@gmail.com>
2013-10-19 20:35:10 +0300

    Add basic view for a book entry

--- books/books/views.py
+++ books/books/views.py
@@ -1,4 +1,12 @@
-from django.views.generic import TemplateView
+
+from django.views.generic import TemplateView, DetailView
+from models import Book
 
 class Overview(TemplateView):
     template_name = 'overview.html'
+
+class BookView(DetailView):
+    template_name = 'book.html'
+    model = Book
+    context_object_name = 'book'
+
