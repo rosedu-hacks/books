5a5c59d154fc378d9d061c0892a81342b7d2d166
iuliam <iulia.manda21@gmail.com>
2013-10-20 01:14:06 +0300

    Login and logout finished.

--- books/books/urls.py
+++ books/books/urls.py
@@ -23,6 +23,8 @@ urlpatterns = patterns('',
     url(r'^book/(?P<pk>\d+)/$', BookView.as_view(), name='book'),
     url(r'login/', 'django.contrib.auth.views.login',
         {'template_name': 'login.html'}),
+    url(r'logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(v1_api.urls)),
 )
