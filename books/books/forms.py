0e39043ec8b88a3ec68f0a113cba78474bbb061a
Mihai Bivol <mm.bivol@gmail.com>
2013-10-20 03:20:54 +0300

    Add form for getting a book

--- books/books/forms.py
+++ books/books/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.forms.extras.widgets import SelectDateWidget
 from models import Person
 
 class RegisterForm(UserCreationForm):
@@ -14,4 +16,18 @@ class RegisterForm(UserCreationForm):
         person.set_password(password)
         person.save()
         return person
+
+class GetBookForm(forms.Form):
+    return_date = forms.DateField(widget=SelectDateWidget)
+
+    def __init__(self, *args, **kwargs):
+        if 'book' not in kwargs:
+            return
+
+        self.book = kwargs.pop('book')
+        super(GetBookForm, self).__init__(*args, **kwargs)
+        shared_by = self.book.shared_by.all()
+        self.fields['persons'] = forms.ChoiceField(widget=forms.Select,
+                                                   choices=[(u.pk, u.name) for u in shared_by])
+
 
--- books/books/views.py
+++ books/books/views.py
@@ -1,11 +1,15 @@
 from django.views.generic import TemplateView
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from books.forms import RegisterForm
 from django.views.generic import FormView, DetailView
 from django.shortcuts import redirect, render
-from books.models import Person
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from books.models import Person, Rental
 
 from models import Book
+from forms import GetBookForm
 
 class Overview(TemplateView):
     template_name = 'overview.html'
@@ -38,4 +42,31 @@ class BookView(DetailView):
     template_name = 'book.html'
     model = Book
     context_object_name = 'book'
+
+class GetBookView(FormView):
+    template_name = 'getbook.html'
+    form_class = GetBookForm
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        self.book = Book.objects.get(pk=kwargs['pk'])
+        return super(GetBookView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        pk = request.POST.get('persons', [None])[0]
+        if not pk:
+            return redirect(reverse('profile', kwargs={'pk': request.user.pk}))
+
+        person = Person.objects.get(pk=pk)
+        person.shared.remove(self.book)
+        to = Person.objects.get(pk=request.user.pk)
+
+        Rental.objects.create(by=person, to=to, book=self.book)
+        return redirect(reverse('profile', kwargs={'pk': request.user.pk}))
+
+    def get_context_data(self, **kwargs):
+        context = super(GetBookView, self).get_context_data(**kwargs)
+        context['bookform'] = GetBookForm(book=self.book)
+        context['book'] = self.book
+        return context
 
